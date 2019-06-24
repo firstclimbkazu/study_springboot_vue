0b41b2a712af36b0ed7e1f0833307b778027ce50
firstclimbkazu <firstclimbkazu@gmail.com>
2019-06-25 07:33:50 +0900

    api 実装

--- demo/src/main/java/demo/domain/adapter/AsyncApiAdapter.java
+++ demo/src/main/java/demo/domain/adapter/AsyncApiAdapter.java
@@ -1,10 +1,17 @@
 package demo.domain.adapter;
 
+
 import java.io.IOException;
 import java.lang.InterruptedException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+
 import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,5 +65,86 @@ public class AsyncApiAdapter {
             e.printStackTrace();
         }
         return CompletableFuture.completedFuture(text);
+    }
+
+    public CompletableFuture<String> executeGet() {
+        String text = new String();
+        System.out.println("===== HTTP GET Start =====");
+        try {
+            URL url = new URL("http://localhost:8080/get?param=value");
+
+            HttpURLConnection connection = null;
+
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
+                                                                       StandardCharsets.UTF_8);
+                         BufferedReader reader = new BufferedReader(isr)) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    }
+                }
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("===== HTTP GET End =====");
+        return CompletableFuture.completedFuture(text);
+
+    }
+
+    public CompletableFuture<String> executePost() {
+        String text = new String();
+        System.out.println("===== HTTP POST Start =====");
+        try {
+            URL url = new URL("http://localhost:8080/post");
+
+            HttpURLConnection connection = null;
+
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(),
+                                                                                  StandardCharsets.UTF_8));
+                writer.write("POST Body");
+                writer.write("\r\n");
+                writer.write("Hello Http Server!!");
+                writer.write("\r\n");
+                writer.flush();
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
+                                                                       StandardCharsets.UTF_8);
+                         BufferedReader reader = new BufferedReader(isr)) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    }
+                }
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("===== HTTP POST End =====");
+        return CompletableFuture.completedFuture(text);
+
     }
 }
